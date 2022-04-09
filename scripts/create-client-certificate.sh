ef36f02aa0efe9790f03714929d8a174ba397fb6
halilbahar <halil.s.bahar@gmail.com>
2022-04-10 00:52:32 +0200

    Initalize ecs-deployment repo

---
+++ scripts/create-client-certificate.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+usage(){
+    cat << EOF
+Usage:  $0 user-email@address.domain cert_name [daysvalid]
+
+Creates a client certificate, and send certificate via Email.
+
++ The the user must already exist
+    and must be internal (ec-office or ec-executive or ec-signing)
++ The default certificate lifetime is 7 days.
+    Change this with supplying daysvalid
++ Requirements: Appliance must be running.
+
+EOF
+    exit 1
+}
+
+daysvalid=7
+email="$1"
+certname="$2"
+if test "$2" = ""; then usage; fi
+if test "$3" != ""; then daysvalid=$3; fi
+
+cat << EOF | docker exec -i ecs_ecs.web_1 /start run ./manage.py shell
+
+email="$email"; certname="$certname"; daysvalid=$daysvalid;
+
+import os
+from django.conf import settings
+from django.contrib.auth.models import Group, User
+from ecs.pki.models import Certificate
+from ecs.communication.mailutils import deliver
+
+if not User.objects.filter(email=email).exists():
+    print("Error: could not find user {}".format(email))
+    exit(1)
+
+u = User.objects.get(email=email)
+cert, passphrase = Certificate.create_for_user(
+    '/tmp/user.p12', u, cn=certname, days=daysvalid)
+pkcs12 = open('/tmp/user.p12', 'rb').read()
+# os.remove('/tmp/user.p12')
+
+deliver(u.email,
+    subject='Certificate {}'.format(certname),
+    message='See attachment',
+    from_email=settings.DEFAULT_FROM_EMAIL,
+    attachments=(('{}.p12'.format(certname), pkcs12, 'application/x-pkcs12'),),
+    nofilter=True)
+
+print("Create and send certificate for {} using passphrase {}".format(email, passphrase))
+exit()
+
+EOF
---
+++ scripts/create-internal-user.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+usage(){
+    cat << EOF
+Usage:  $0 email@domain "first name" "Last Name" "f/m"
+
+Creates a internal office user.
+Gender can be "f" or "m".
+Requirements: Appliance must be running.
+
+EOF
+    exit 1
+}
+
+email="$1"
+first="$2"
+last="$3"
+gender=$(echo "$4" | tr '[:upper:]' '[:lower:]')
+if test "$gender" != "m" -a "$gender" != "f"; then
+    usage
+fi
+
+cat << EOF | docker exec -i ecs_ecs.web_1 /start run ./manage.py shell
+
+email='$email'; first_name='$first'; last_name='$last'; gender='$gender'
+
+import math, string
+from random import SystemRandom
+from django.contrib.auth.models import Group, User
+from ecs.users.utils import create_user
+
+PASSPHRASE_ENTROPY = 80
+PASSPHRASE_CHARS = string.ascii_lowercase + string.digits
+PASSPHRASE_LEN = math.ceil(PASSPHRASE_ENTROPY / math.log2(len(PASSPHRASE_CHARS)))
+
+u = User.objects.get(email=email) if User.objects.filter(email=email).exists() else create_user(email)
+p = u.profile
+p.gender = gender
+p.is_internal = True
+p.save()
+passphrase = ''.join(SystemRandom().choice(PASSPHRASE_CHARS) for i in range(PASSPHRASE_LEN))
+u.first_name=first_name
+u.last_name=last_name
+u.set_password(passphrase)
+print("created/updated user {} with passphrase {}".format (email, passphrase))
+u.save()
+u.groups.add(Group.objects.get(name='EC-Office'))
+exit()
+
+EOF
