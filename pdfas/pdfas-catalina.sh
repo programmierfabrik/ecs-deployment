87532c9d4cf44a3708117bd76bb849cd6c593b11
halilbahar <halil.s.bahar@gmail.com>
2022-09-08 01:53:22 +0200

    Remove faulty chain copying

--- pdfas/pdfas-catalina.sh
+++ pdfas/pdfas-catalina.sh
@@ -4,10 +4,6 @@ set -e
 config=/app/pdf-as-web/pdf-as-web.properties
 sed "s/HOSTNAME/$HOSTNAME/g" ${config}.tmpl > ${config}
 
-# copy server cert to system cert import location
-cp /app/import/acme/live/$HOSTNAME/fullchain.pem /usr/local/share/ca-certificates/server.crt
-chmod 0600 /usr/local/share/ca-certificates/server.crt
-
 # Disable expired let's encrypt certificates and download the new one
 sed -i -e 's|mozilla/DST_Root_CA_X3.crt|#mozilla/DST_Root_CA_X3.crt|g' /etc/ca-certificates.conf
 sed -i -e 's|mozilla/ISRG_Root_X1.crt|#mozilla/ISRG_Root_X1.crt|g' /etc/ca-certificates.conf
