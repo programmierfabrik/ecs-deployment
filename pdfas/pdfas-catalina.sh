3cd2695d23d99d5766f0027befc1ce609bde7204
halilbahar <halil.s.bahar@gmail.com>
2022-09-08 01:41:27 +0200

    Fix ssl issue with pdfas

--- pdfas/pdfas-catalina.sh
+++ pdfas/pdfas-catalina.sh
@@ -8,10 +8,12 @@ sed "s/HOSTNAME/$HOSTNAME/g" ${config}.tmpl > ${config}
 cp /app/import/acme/live/$HOSTNAME/fullchain.pem /usr/local/share/ca-certificates/server.crt
 chmod 0600 /usr/local/share/ca-certificates/server.crt
 
-# copy letsencrypt ISRG Root X1 cert to system cert import location
-cp /app/import/isrgrootx1.pem /usr/local/share/ca-certificates/isrgrootx1.pem
+# Disable expired let's encrypt certificates and download the new one
+sed -i -e 's|mozilla/DST_Root_CA_X3.crt|#mozilla/DST_Root_CA_X3.crt|g' /etc/ca-certificates.conf
+sed -i -e 's|mozilla/ISRG_Root_X1.crt|#mozilla/ISRG_Root_X1.crt|g' /etc/ca-certificates.conf
+wget --no-check-certificate https://letsencrypt.org/certs/isrgrootx1.pem -O /usr/local/share/ca-certificates/isrgrootx1.crt
 
 # update-ca-certificates also updates the java keystore
-/usr/sbin/update-ca-certificates
+/usr/sbin/update-ca-certificates --fresh
 
 exec /usr/local/tomcat/bin/catalina.sh "$@"
