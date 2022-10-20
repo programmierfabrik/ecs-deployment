061086169058b26322214308ee717254424f8c97
halilbahar <halil.s.bahar@gmail.com>
2022-10-20 11:40:53 +0200

    Enable online and local bku

--- pdfas/setenv.sh
+++ pdfas/setenv.sh
@@ -4,5 +4,4 @@ export CATALINA_OPTS="$CATALINA_OPTS \
     -Xmx1024m \
     -Dlogback.configurationFile=$CATALINA_HOME/conf/pdf-as/cfg/logback.xml \
     -Dpdf-as-web.conf=$CATALINA_HOME/conf/pdf-as/pdf-as-web.properties \
-    $EXTRA_OPTS \
 "
